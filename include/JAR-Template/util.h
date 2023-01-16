a4a7f80f0eb9ce537e370a26552c2e48a2b94b02
Josh <joshua.bernheisel@gmail.com>
2023-01-16 01:35:40 -0600

    removed task template

--- include/JAR-Template/util.h
+++ include/JAR-Template/util.h
@@ -1,7 +1,3 @@
-#include <type_traits>
-#include <functional>
-#include <memory>
-
 float reduce_0_to_360(float angle);
 
 float reduce_negative_180_to_180(float angle);
@@ -15,12 +11,3 @@ float to_deg(float angle_rad);
 float clamp(float input, float min, float max);
 
 bool is_reversed(double input);
-
-template <class F>
-vex::task launch_task(F&& function) {
-  return vex::task([](void* parameters) {
-    std::unique_ptr<std::function<void()>> ptr{static_cast<std::function<void()>*>(parameters)};
-    (*ptr)();
-    return 0;
-  }, new std::function<void()>(std::forward<F>(function)));
-}
