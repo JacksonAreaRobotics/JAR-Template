3df775f1197b4ee3d3dd154d12791ee300f940ff
Joshua Bernheisel <joshua.bernheisel@gmail.com>
2023-06-11 14:39:19 -0500

    removed blog

--- site/docusaurus.config.js
+++ site/docusaurus.config.js
@@ -82,7 +82,6 @@ const config = {
             position: 'left',
             label: 'Tutorial',
           },
-          {to: '/blog', label: 'Blog', position: 'left'},
           {
             href: 'https://github.com/facebook/docusaurus',
             label: 'GitHub',
