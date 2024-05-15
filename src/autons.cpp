a70a5a5a6091b7561c41c0323a25d78a5eb91c81
Joshua Bernheisel <joshua.bernheisel@gmail.com>
2024-05-15 19:24:55 -0400

    commented on autons

--- src/autons.cpp
+++ src/autons.cpp
@@ -1,5 +1,13 @@
 #include "vex.h"
 
+/**
+ * Resets the constants for auton movement.
+ * Modify these to change the default behavior of functions like
+ * drive_distance(). For explanations of the difference between
+ * drive, heading, turning, and swinging, as well as the PID and
+ * exit conditions, check the docs.
+ */
+
 void default_constants(){
   // Each constant set is in the form of (maxVoltage, kP, kI, kD, startI).
   chassis.set_drive_constants(10, 1.5, 0, 10, 0);
@@ -7,11 +15,17 @@ void default_constants(){
   chassis.set_turn_constants(12, .4, .03, 3, 15);
   chassis.set_swing_constants(12, .3, .001, 2, 15);
 
-  // Each exit condition set is in the form (settle_error, settle_time, timeout).
+  // Each exit condition set is in the form of (settle_error, settle_time, timeout).
   chassis.set_drive_exit_conditions(1.5, 300, 5000);
   chassis.set_turn_exit_conditions(1, 300, 3000);
   chassis.set_swing_exit_conditions(1, 300, 3000);
 }
+
+/**
+ * Sets constants to be more effective for odom movements.
+ * For functions like drive_to_point(), it's often better to have
+ * a slower max_voltage and greater settle_error than you would otherwise.
+ */
 
 void odom_constants(){
   default_constants();
@@ -19,12 +33,20 @@ void odom_constants(){
   chassis.drive_settle_error = 3;
 }
 
+/**
+ * The expected behavior is to return to the start position.
+ */
+
 void drive_test(){
   chassis.drive_distance(6);
   chassis.drive_distance(12);
   chassis.drive_distance(18);
   chassis.drive_distance(-36);
 }
+
+/**
+ * The expected behavior is to return to the start angle, after making a complete turn.
+ */
 
 void turn_test(){
   chassis.turn_to_angle(5);
@@ -34,10 +56,18 @@ void turn_test(){
   chassis.turn_to_angle(0);
 }
 
+/**
+ * Should swing in a fun S shape.
+ */
+
 void swing_test(){
   chassis.left_swing_to_angle(90);
   chassis.right_swing_to_angle(0);
 }
+
+/**
+ * A little of this, a little of that; it should end roughly where it started.
+ */
 
 void full_test(){
   chassis.drive_distance(24);
@@ -47,6 +77,12 @@ void full_test(){
   chassis.drive_distance(24);
   chassis.turn_to_angle(0);
 }
+
+/**
+ * Doesn't drive the robot, but just prints coordinates to the Brain screen 
+ * so you can check if they are accurate to life. Push the robot around and
+ * see if the coordinates increase like you'd expect.
+ */
 
 void odom_test(){
   chassis.set_coordinates(0, 0, 0);
@@ -61,6 +97,11 @@ void odom_test(){
   }
 }
 
+/**
+ * Should end in the same place it began, but the second movement
+ * will be curved while the first is straight.
+ */
+
 void tank_odom_test(){
   odom_constants();
   chassis.set_coordinates(0, 0, 0);
@@ -69,6 +110,11 @@ void tank_odom_test(){
   chassis.drive_to_point(0,0);
   chassis.turn_to_angle(0);
 }
+
+/**
+ * Drives in a square while making a full turn in the process. Should
+ * end where it started.
+ */
 
 void holonomic_odom_test(){
   odom_constants();
