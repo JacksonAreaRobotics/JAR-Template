ca46813d8a19ef52694eb0038d61f42ffb9fa43a
Joshua Bernheisel <joshua.bernheisel@gmail.com>
2024-05-15 19:04:15 -0400

    commented on odom.cpp

--- src/JAR-Template/odom.cpp
+++ src/JAR-Template/odom.cpp
@@ -1,9 +1,35 @@
 #include "vex.h"
 
+/**
+ * Setter method for tracker center distances.
+ * The forward tracker center distance is the horizontal distance from the 
+ * center of the robot to the center of the wheel the sensor is measuring.
+ * The sideways tracker center distance is the vertical distance from the 
+ * center of the robot to the center of the sideways wheel being measured.
+ * If there's really no sideways wheel we set the center distance to 0 and
+ * pretend the wheel never spins, which is equivalent to a no-drift robot.
+ * 
+ * @param ForwardTracker_center_distance A horizontal distance to the wheel center in inches.
+ * @param SideWaysTracker_center_distance A vertical distance to the wheel center in inches.
+ */
+
 void Odom::set_physical_distances(float ForwardTracker_center_distance, float SidewaysTracker_center_distance){
   this->ForwardTracker_center_distance = ForwardTracker_center_distance;
   this->SidewaysTracker_center_distance = SidewaysTracker_center_distance;
 }
+
+/**
+ * Resets the position, including tracking wheels.
+ * Position is field-centric, and orientation is such that 0 degrees
+ * is in the positive Y direction. Orientation can be provided with 
+ * some flexibility, including less than 0 and greater than 360.
+ * 
+ * @param X_position Field-centric x position of the robot.
+ * @param Y_position Field-centric y position of the robot.
+ * @param orientation_deg Field-centered, clockwise-positive, orientation.
+ * @param ForwardTracker_position Current position of the sensor in inches.
+ * @param SidewaysTracker_position Current position of the sensor in inches.
+ */
 
 void Odom::set_position(float X_position, float Y_position, float orientation_deg, float ForwardTracker_position, float SidewaysTracker_position){
   this->ForwardTracker_position = ForwardTracker_position;
@@ -12,6 +38,18 @@ void Odom::set_position(float X_position, float Y_position, float orientation_de
   this->Y_position = Y_position;
   this->orientation_deg = orientation_deg;
 }
+
+/**
+ * Does the odometry math to update position
+ * Uses the Pilons arc method outline here: https://wiki.purduesigbots.com/software/odometry
+ * All the deltas are done by getting member variables and comparing them to 
+ * the input. Ultimately this all works to update the public member variable
+ * X_position. This function needs to be run at 200Hz or so for best results.
+ * 
+ * @param ForwardTracker_position Current position of the sensor in inches.
+ * @param SidewaysTracker_position Current position of the sensor in inches.
+ * @param orientation_deg Field-centered, clockwise-positive, orientation.
+ */
 
 void Odom::update_position(float ForwardTracker_position, float SidewaysTracker_position, float orientation_deg){
   // this-> always refers to the old version of the variable, so subtracting this->x from x gives delta x.
@@ -26,9 +64,6 @@ void Odom::update_position(float ForwardTracker_position, float SidewaysTracker_
 
   float local_X_position;
   float local_Y_position;
-
-  // All of the following lines are pretty well documented in 5225A's Into to Position Tracking 
-  // Document at http://thepilons.ca/wp-content/uploads/2018/10/Tracking.pdf 
 
   if (orientation_delta_rad == 0) {
     local_X_position = Sideways_delta;
